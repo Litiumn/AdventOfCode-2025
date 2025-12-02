0d99f52db6bbf8455db16f55af3b3c850c6efe2e
Litiumn <adrianjaredsido@gmail.com>
2025-12-02 12:48:52 +0800

    Day 1 solution

---
+++ day_1/Part_1.py
@@ -0,0 +1,24 @@
+file = open('file.txt', 'r')
+num = 50
+password = 0
+for line in file:
+    letter = line.strip()
+    previousNum = num
+    rotations = 0
+
+    if(letter[0] == 'L'):
+        num -= int(letter.split('L')[1])
+    else:
+        num += int(letter.split('R')[1])
+
+    rotations += abs(num)//100
+
+    if((num < 0 and previousNum != 0) or num == 0):
+        rotations +=1
+
+    password += rotations
+    num %= 100
+
+print(password)
+
+
