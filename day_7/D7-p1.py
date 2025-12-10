f04c5cfc3ec2967d95043b985fa4615ef3f11e46
Litiumn <adrianjaredsido@gmail.com>
2025-12-10 13:12:56 +0800

    Day 7 solution

---
+++ day_7/D7-p1.py
@@ -0,0 +1,23 @@
+with open('/home/adrian/Documents/Programming/Advent_of_Code/day_7/test.txt', 'r') as file:
+    arr = []
+    for line in file:
+        arr.append(line.strip())
+
+def traverse(arr, x, y, visited, sum = 0):
+    if (x, y) in visited:
+        return 0
+    visited.add((x, y))
+
+    if arr[x][y] == '^':
+        sum += traverse(arr, x, y-1, visited)
+        sum += traverse(arr, x, y+1, visited)
+        sum += 1
+        return sum
+    if x == len(arr) - 1:
+        return 0
+    if arr[x][y] == '.':
+        sum += traverse(arr, x+1, y, visited)
+    return sum
+
+visited = set()
+print(traverse(arr, 1, len(arr[0])//2, visited))
---
+++ day_7/D7-p2.py
@@ -0,0 +1,20 @@
+with open('/home/adrian/Documents/Programming/Advent_of_Code/day_7/test.txt', 'r') as file:
+    arr = []
+    for line in file:
+        arr.append(line.strip())
+
+def traverse(arr, x, y, memo, sum = 0):
+    if (x,y) in memo:
+        return memo[(x,y)]
+    
+    if arr[x][y] == '^':
+        memo[(x,y)] = traverse(arr, x, y-1, memo) + traverse(arr, x, y+1, memo)
+        return memo[(x,y)]
+    if x == len(arr) - 1:
+        return 1
+    if arr[x][y] == '.':
+        memo[(x,y)] = traverse(arr, x+1, y, memo)
+    return memo[(x,y)]
+
+memo = dict()
+print(traverse(arr, 1, len(arr[0])//2, memo))
